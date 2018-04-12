fe661938e9590ff9178232c769e0bd5eb8694a13
Kenz-16 <mcp5314@psu.edu>
2018-04-12 13:49:29 -0400

    test kenz

--- src/thursdaytest/ThursdayTest.java
+++ src/thursdaytest/ThursdayTest.java
@@ -15,7 +15,7 @@ public class ThursdayTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String testKenz = "Kenz Test";
     }
     
 }
